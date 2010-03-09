9b5513c8f88e56b80f0506e04e831663ec9318b4
(no author) <(no author)@b53afa6a-2257-11df-8f40-0d449d17a094>
2010-03-09 04:58:00 +0000

    git-svn-id: https://magja.googlecode.com/svn/trunk@61 b53afa6a-2257-11df-8f40-0d449d17a094

--- src/main/java/code/google/magja/soap/SoapReturnParser.java
+++ src/main/java/code/google/magja/soap/SoapReturnParser.java
@@ -43,6 +43,7 @@ public class SoapReturnParser {
 
 	@SuppressWarnings("unchecked")
 	private void initNamespaces(SOAPEnvelope env) {
+		//test
 		int nsFound = 0;
 		for (Iterator<OMNamespace> iNamespaces = env.getAllDeclaredNamespaces(); iNamespaces
 				.hasNext();) {
