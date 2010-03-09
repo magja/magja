b4516e5847b49ecab29ef684261f0873ce992290
(no author) <(no author)@b53afa6a-2257-11df-8f40-0d449d17a094>
2010-03-09 04:57:58 +0000

    git-svn-id: https://magja.googlecode.com/svn/trunk@58 b53afa6a-2257-11df-8f40-0d449d17a094

---
+++ src/test/java/code/google/magja/magento/product/ProductConfigurableTest.java
@@ -0,0 +1,26 @@
+package code.google.magja.magento.product;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class ProductConfigurableTest extends ProductAbstractTest {
+
+	@Test
+	public void testCreateProduct() {
+		
+		// first, we have to create a new attribute set
+		
+		
+		
+		
+		Product p = new Product();
+		ProductProperties mpp = generateProductProperties();
+
+		Integer productId = p.create("CONF-PRD-TEST", ProductType.CONFIGURABLE, mpp, 4);
+		assertFalse((new Integer(-1)).equals(productId));
+
+		p.logout();
+	}
+
+}
--- src/test/java/code/google/magja/magento/product/ProductTest.java
+++ src/test/java/code/google/magja/magento/product/ProductTest.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package code.google.magja.magento.product;
 
 import static org.junit.Assert.assertFalse;
@@ -8,11 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * @author andre
- *
- */
-public class ProductTest {
+public class ProductTest extends ProductAbstractTest {
 
 	private static final String SKU_PRODUCT = "PRDAUTOTEST";
 
@@ -27,6 +20,21 @@ public class ProductTest {
 	public void testCreateProduct() {
 		Product p = new Product();
 		productId = p.create(SKU_PRODUCT);
+		assertFalse((new Integer(-1)).equals(productId));
+		p.logout();
+	}
+
+	/**
+	 * Test method for
+	 * {@link code.google.magja.magento.product.Product#create(java.lang.String, code.google.magja.magento.product.ProductType, code.google.magja.magento.product.ProductProperties, int)}
+	 * .
+	 */
+	@Test
+	public void testCreateStringProductTypeProductPropertiesInt() {
+		Product p = new Product();
+		ProductProperties mpp = generateProductProperties();
+
+		productId = p.create(SKU_PRODUCT, ProductType.SIMPLE, mpp, 1);
 		assertFalse((new Integer(-1)).equals(productId));
 		p.logout();
 	}
@@ -75,21 +83,6 @@ public class ProductTest {
 
 	/**
 	 * Test method for
-	 * {@link code.google.magja.magento.product.Product#create(java.lang.String, code.google.magja.magento.product.ProductType, code.google.magja.magento.product.ProductProperties, int)}
-	 * .
-	 */
-	@Test
-	public void testCreateStringProductTypeProductPropertiesInt() {
-		Product p = new Product();
-		ProductProperties mpp = generateProductProperties();
-
-		productId = p.create(SKU_PRODUCT, ProductType.SIMPLE, mpp, 1);
-		assertFalse((new Integer(-1)).equals(productId));
-		p.logout();
-	}
-
-	/**
-	 * Test method for
 	 * {@link code.google.magja.magento.product.Product#delete(java.lang.String)}
 	 * .
 	 */
@@ -121,26 +114,6 @@ public class ProductTest {
 		// then delete created
 		assertTrue(p.delete(productId));
 		p.logout();
-	}
-
-	private ProductProperties generateProductProperties() {
-		ProductProperties mpp = new ProductProperties();
-		mpp.setName("Remote Testing Inserting Product");
-		mpp.setDescription("This is a Description of remote " +
-				"testing to insert a new Product");
-		mpp.setShort_description("This is a Short Description of " +
-				"remote testing to insert a new Product");
-
-		mpp.setPrice(new Double(120.34));
-		mpp.set("cost", new Double(64.68));
-
-		mpp.setStatus(1);
-		mpp.setWeight(new Double(0.300));
-		int[] websites = { 1 };
-		mpp.setWebsites(websites);
-		mpp.setTax_class_id(1);
-
-		return mpp;
 	}
 
 }
