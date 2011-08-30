fc1f347e4dc1dd0e865b6a3801dd263df9e556d7
andre@rockstore.com.br <andre@rockstore.com.br@b53afa6a-2257-11df-8f40-0d449d17a094>
2011-08-30 02:54:13 +0000

    git-svn-id: https://magja.googlecode.com/svn/trunk@390 b53afa6a-2257-11df-8f40-0d449d17a094

--- src/main/java/com/google/code/magja/service/product/ProductServiceUtil.java
+++ src/main/java/com/google/code/magja/service/product/ProductServiceUtil.java
@@ -4,18 +4,19 @@ import com.google.code.magja.model.product.Product;
 
 /**
  * @author andrefabbro
- *
+ * 
  */
 public class ProductServiceUtil {
-	
+
 	/**
 	 * test if the product is not null, and if the product id or sku is not null
-	 *
+	 * 
 	 * @param product
 	 * @return
 	 */
 	protected static Boolean validateProduct(Product product) {
-		return product != null && (product.getId() != null || product.getSku() != null);
+		return product != null
+				&& (product.getId() != null || product.getSku() != null);
 	}
 
 }
