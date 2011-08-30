ade0630b4f1e494e6d3c2cb175ff96b49e986e4c
andre@rockstore.com.br <andre@rockstore.com.br@b53afa6a-2257-11df-8f40-0d449d17a094>
2011-08-30 02:15:47 +0000

    changes needed for use that class as key for map of MagentoSoapClient instances
    git-svn-id: https://magja.googlecode.com/svn/trunk@388 b53afa6a-2257-11df-8f40-0d449d17a094

--- src/main/java/com/google/code/magja/soap/SoapConfig.java
+++ src/main/java/com/google/code/magja/soap/SoapConfig.java
@@ -1,8 +1,11 @@
 package com.google.code.magja.soap;
 
+import java.io.Serializable;
 import java.util.Properties;
 
-public class SoapConfig {
+public class SoapConfig implements Serializable {
+
+	private static final long serialVersionUID = 6393846699827611865L;
 
 	public static final String MAGENTO_API_PASSWORD = "magento-api-password";
 
@@ -24,7 +27,7 @@ public class SoapConfig {
 
 	private Integer defaultRootCategoryId;
 
-	public SoapConfig(String apiUser, String apiKey, String remoteHost){
+	public SoapConfig(String apiUser, String apiKey, String remoteHost) {
 		this.apiUser = apiUser;
 		this.apiKey = apiKey;
 		this.remoteHost = remoteHost;
@@ -32,12 +35,14 @@ public class SoapConfig {
 		this.defaultRootCategoryId = 2;
 	}
 
-	public SoapConfig(Properties properties){
+	public SoapConfig(Properties properties) {
 		this.apiUser = properties.getProperty(MAGENTO_API_USERNAME);
 		this.apiKey = properties.getProperty(MAGENTO_API_PASSWORD);
 		this.remoteHost = properties.getProperty(MAGENTO_API_URL);
-		this.defaultAttributeSetId = Integer.parseInt(properties.getProperty(DEFAULT_ATTRIBUTE_SET_ID));
-		this.defaultRootCategoryId = Integer.parseInt(properties.getProperty(DEFAULT_ROOT_CATEGORY_ID));
+		this.defaultAttributeSetId = Integer.parseInt(properties
+				.getProperty(DEFAULT_ATTRIBUTE_SET_ID));
+		this.defaultRootCategoryId = Integer.parseInt(properties
+				.getProperty(DEFAULT_ROOT_CATEGORY_ID));
 	}
 
 	/**
@@ -113,5 +118,84 @@ public class SoapConfig {
 	 */
 	public void setDefaultRootCategoryId(Integer defaultRootCategoryId) {
 		this.defaultRootCategoryId = defaultRootCategoryId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apiKey == null) ? 0 : apiKey.hashCode());
+		result = prime * result + ((apiUser == null) ? 0 : apiUser.hashCode());
+		result = prime
+				* result
+				+ ((defaultAttributeSetId == null) ? 0 : defaultAttributeSetId
+						.hashCode());
+		result = prime
+				* result
+				+ ((defaultRootCategoryId == null) ? 0 : defaultRootCategoryId
+						.hashCode());
+		result = prime * result
+				+ ((remoteHost == null) ? 0 : remoteHost.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SoapConfig other = (SoapConfig) obj;
+		if (apiKey == null) {
+			if (other.apiKey != null)
+				return false;
+		} else if (!apiKey.equals(other.apiKey))
+			return false;
+		if (apiUser == null) {
+			if (other.apiUser != null)
+				return false;
+		} else if (!apiUser.equals(other.apiUser))
+			return false;
+		if (defaultAttributeSetId == null) {
+			if (other.defaultAttributeSetId != null)
+				return false;
+		} else if (!defaultAttributeSetId.equals(other.defaultAttributeSetId))
+			return false;
+		if (defaultRootCategoryId == null) {
+			if (other.defaultRootCategoryId != null)
+				return false;
+		} else if (!defaultRootCategoryId.equals(other.defaultRootCategoryId))
+			return false;
+		if (remoteHost == null) {
+			if (other.remoteHost != null)
+				return false;
+		} else if (!remoteHost.equals(other.remoteHost))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "SoapConfig [apiUser=" + apiUser + ", apiKey=" + apiKey
+				+ ", remoteHost=" + remoteHost + ", defaultAttributeSetId="
+				+ defaultAttributeSetId + ", defaultRootCategoryId="
+				+ defaultRootCategoryId + "]";
 	}
 }
