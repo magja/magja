e98aaabec4f5c8f4dad43929a7a07f66630eab63
Simon Zambrovski <simon.zambrovski@holisticon.de>
2017-01-11 01:57:08 +0100

    Fixed test

--- src/test/java/com/google/code/magja/model/product/ProductMediaTest.java
+++ src/test/java/com/google/code/magja/model/product/ProductMediaTest.java
@@ -3,68 +3,44 @@
  */
 package com.google.code.magja.model.product;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.code.magja.utils.MagjaFileUtils;
 
 /**
+ * Tests the FileUtils.
+ * 
  * @author andre
- *
  */
-@Ignore
 public class ProductMediaTest {
+
+  private String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/"
+      + "Magento_2_Admin_Panel_screenshot.png/1280px-Magento_2_Admin_Panel_screenshot.png";
+  private final static Logger log = LoggerFactory.getLogger(ProductMediaTest.class);
 
   @Test
   public void testImageFileIThree() throws Exception {
-
-    String url = "http://www.rockstore.com.br/store/catalog/GV003_foto.jpg";
-    byte[] data = MagjaFileUtils.getBytesFromFileURL(url);
+    byte[] data = MagjaFileUtils.getBytesFromFileURL(imageUrl);
     assertNotNull(data);
-  }
-
-  @Test
-  public void testImageFileIOne() throws Exception {
-
-    String url = "http://www.rockstore.com.br/store/catalog/GV003_foto.jpg";
-    byte[] data = MagjaFileUtils.getBytesFromFileURL(url);
-    assertNotNull(data);
-    // String filename = u.getFile().substring(url.lastIndexOf('/') + 1);
-    // String filename = "/home/andre/DEV/temp/image.jpg";
-    // FileOutputStream out = new FileOutputStream(filename);
-    // out.write(data);
-    // out.flush();
-    // out.close();
-
   }
 
   @Test
   public void testImageFileTwo() throws Exception {
-    BufferedImage image = null;
-
-    // Read from a file
-    // File file = new File("image.gif");
-    // image = ImageIO.read(file);
-
-    // Read from an input stream
-    // InputStream is = new BufferedInputStream( new
-    // FileInputStream("image.gif"));
-    // image = ImageIO.read(is);
-
-    // Read from a URL
-    URL url = new URL("http://www.rockstore.com.br/store/catalog/GV003_foto.jpg");
-    image = ImageIO.read(url);
-    System.out.println(image.toString());
-    ImageInputStream iis = ImageIO.createImageInputStream(image);
-    assertNotNull(iis);
+    BufferedImage image = ImageIO.read(new URL(imageUrl));
+    log.info("{}", image);
+    assertTrue(image.getHeight() != 0);
+    assertTrue(image.getWidth() != 0);
+    byte[] data = MagjaFileUtils.getBytesFromBufferedImage(image, "PNG");
+    assertNotNull(data);
   }
 
 }
