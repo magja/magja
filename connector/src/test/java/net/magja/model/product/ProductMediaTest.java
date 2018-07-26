64f248b875c39db47c13f83f1ba3b35a013c188b
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-07-26 11:38:15 +0200

    Improved test and changed URL

--- connector/src/test/java/net/magja/model/product/ProductMediaTest.java
+++ connector/src/test/java/net/magja/model/product/ProductMediaTest.java
@@ -11,6 +11,7 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,26 +25,34 @@ import net.magja.utils.MagjaFileUtils;
  */
 public class ProductMediaTest {
 
-  private String imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Ambox_important.svg/40px-Ambox_important.svg.png";
   private final static Logger log = LoggerFactory.getLogger(ProductMediaTest.class);
+  private final static String imageUrl = "https://de.wikipedia.org/wiki/Portable_Network_Graphics#/media/File:Png-logo.png";
 
-  @Test
-  public void testImageFileIThree() throws Exception {
-    byte[] data = MagjaFileUtils.getBytesFromFileURL(imageUrl);
-    assertNotNull(data);
+  private BufferedImage image;
+
+  @Before
+  public void preCheck() throws Exception {
+    // given
+    image = ImageIO.read(new URL(imageUrl));
+    assertTrue("Image is readable and has a size.", image != null && image.getHeight() != 0 && image.getWidth() != 0);
   }
 
   @Test
   public void testImageFileTwo() throws Exception {
-    // given
-    BufferedImage image = ImageIO.read(new URL(imageUrl));
-    assertTrue("Image is readable and has a size.", image != null && image.getHeight() != 0 && image.getWidth() != 0);
-
     // when
     byte[] data = MagjaFileUtils.getBytesFromBufferedImage(image, "PNG");
-
     // then
     assertNotNull(data);
   }
+
+
+  @Test
+  public void testImageFileThree() throws Exception {
+    // when
+    byte[] data = MagjaFileUtils.getBytesFromFileURL(imageUrl);
+    // then
+    assertNotNull(data);
+  }
+
 
 }
