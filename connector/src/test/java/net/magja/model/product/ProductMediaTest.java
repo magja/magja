521454ea5bf0cc535e52b1d5d30dc2ccd0b5b8e6
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-07-26 11:48:39 +0200

    Switched to local file for testing

--- connector/src/test/java/net/magja/model/product/ProductMediaTest.java
+++ connector/src/test/java/net/magja/model/product/ProductMediaTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -26,19 +28,26 @@ import net.magja.utils.MagjaFileUtils;
 public class ProductMediaTest {
 
   private final static Logger log = LoggerFactory.getLogger(ProductMediaTest.class);
-  private final static String imageUrl = "https://de.wikipedia.org/wiki/Portable_Network_Graphics#/media/File:Png-logo.png";
 
+  // = "https://de.wikipedia.org/wiki/Portable_Network_Graphics#/media/File:Png-logo.png";
+
+  private URL imageUrl;
   private BufferedImage image;
+
 
   @Before
   public void preCheck() throws Exception {
+
+    final File imageFile = new File("src/test/resources/CC-BY-SA_icon.svg.png");
+    this.imageUrl = imageFile.toURI().toURL();
+
     // given
-    image = ImageIO.read(new URL(imageUrl));
+    image = ImageIO.read(imageUrl);
     assertTrue("Image is readable and has a size.", image != null && image.getHeight() != 0 && image.getWidth() != 0);
   }
 
   @Test
-  public void testImageFileTwo() throws Exception {
+  public void testImageFileTwo() {
     // when
     byte[] data = MagjaFileUtils.getBytesFromBufferedImage(image, "PNG");
     // then
@@ -47,9 +56,9 @@ public class ProductMediaTest {
 
 
   @Test
-  public void testImageFileThree() throws Exception {
+  public void testImageFileThree() throws IOException {
     // when
-    byte[] data = MagjaFileUtils.getBytesFromFileURL(imageUrl);
+    byte[] data = MagjaFileUtils.getBytesFromFileURL(imageUrl.toString());
     // then
     assertNotNull(data);
   }
